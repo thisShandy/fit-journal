68c8205b6c2e56be0a955e0b4c82f290853ea63c
thisShandy <shandyrix@gmail.com>
2025-08-02 22:15:56 +0200

    feat: updating request.ts

--- client/src/common/core/i18n/request.ts
+++ client/src/common/core/i18n/request.ts
@@ -1,10 +1,21 @@
+import { cookies, headers } from "next/headers";
 import { getRequestConfig } from "next-intl/server";
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  const requested = await requestLocale;
+export default getRequestConfig(async () => {
+  const requestHeaders = await headers();
+  const requestCookies = await cookies();
 
-  console.log("event", requested);
+  const localeFromCookie = requestCookies.get("locale")?.value;
+  const acceptLanguage = requestHeaders.get("accept-language");
+  const localeFromHeader = acceptLanguage?.split(",")[0];
+
   const locale = "en";
+
+  const ip = requestHeaders.get("x-forwarded-for")?.split(",")[0]
+    || requestHeaders.get("x-real-ip")
+    || undefined;
+
+  console.log("config", localeFromCookie, localeFromHeader, locale, ip);
 
   return {
     locale,
