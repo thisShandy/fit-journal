4e2681a88460c3c9bd7cddb930a2cc97bb1c0148
quland <jolygolfq@gmail.com>
2025-08-08 23:09:27 +0300

    feat: add reviews

--- client/src/common/ui/layout/article-layout/index.tsx
+++ client/src/common/ui/layout/article-layout/index.tsx
@@ -3,6 +3,7 @@ import type { FC, PropsWithChildren } from "react";
 import ArticleFooter from "~/common/ui/component/article-footer";
 
 import style from "./style/style.module.scss";
+import ArticleReviews from "~/common/ui/component/article-reviews";
 
 interface IArticleLayoutProps {
   footerText?: string;
@@ -14,6 +15,8 @@ const ArticleLayout: FC<PropsWithChildren<IArticleLayoutProps>> = ({
   return (
     <div className={style.articleLayout}>
       {children}
+      <ArticleReviews avatar={"https://cdn.vokrug.tv/pic/person/2/b/f/4/2bf448098b7badf3b37e87c510da29bc.jpeg"} name={"Anastasiya"} time={"23 minutes"} text={"lorem ipsumawdin lorem ipsumawdin lorem ipsumawdin lorem ipsumawdin lorem ipsumawdin "}/>
+      <ArticleReviews avatar={"https://cdn.vokrug.tv/pic/person/2/b/f/4/2bf448098b7badf3b37e87c510da29bc.jpeg"} name={"Anton"} time={"1 hour"} text={"lorem ipsumawdin lorem ipsumawdin lorem ipsumawdin lorem ipsumawdin lorem ipsumawdin "} imgUrl={"https://cdn.7days.ru/pic/267/985401/1466051/86.jpg"}/>
       <ArticleFooter text={footerText} />
     </div>
   );
