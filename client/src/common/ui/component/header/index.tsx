9d807b6a131faaecb286e006f5acf73c13f69948
thisShandy <shandyrix@gmail.com>
2025-08-09 13:49:33 +0200

    feat: header mobile adaptation

--- client/src/common/ui/component/header/index.tsx
+++ client/src/common/ui/component/header/index.tsx
@@ -5,21 +5,25 @@ import style from "./style/style.module.scss";
 const Header = () => {
   return (
     <header className={style.header}>
-      <span className={style.header__logo}>FitJornual</span>
-      <ul className={style.header__topics}>
-        <li className={style.header__topics_item}>
-          <Link href="/fit">Похудение</Link>
-        </li>
-        <li className={style.header__topics_item}>
-          <Link href="/bone">Суставы</Link>
-        </li>
-        <li className={style.header__topics_item}>
-          <Link href="/sugar">Сахар</Link>
-        </li>
-        <li className={style.header__topics_item}>
-          <Link href="/young">Омоложение</Link>
-        </li>
-      </ul>
+      <div className={style.headerContainer}>
+        <Link href="/" className={style.headerLogo}>
+          <span className={style.headerLogo__title}>FitJornual</span>
+        </Link>
+        <ul className={style.headerTopics}>
+          <li className={style.headerTopics__item}>
+            <Link href="/fit">Похудение</Link>
+          </li>
+          <li className={style.headerTopics__item}>
+            <Link href="/bone">Суставы</Link>
+          </li>
+          <li className={style.headerTopics__item}>
+            <Link href="/sugar">Сахар</Link>
+          </li>
+          <li className={style.headerTopics__item}>
+            <Link href="/young">Омоложение</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
