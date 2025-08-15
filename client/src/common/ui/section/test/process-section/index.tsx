aedb131f95c5d3c7bb698c3f193e5e78ee500ccd
thisShandy <shandyrix@gmail.com>
2025-08-15 13:26:53 +0200

    feat: progress section updates

--- client/src/common/ui/section/test/process-section/index.tsx
+++ client/src/common/ui/section/test/process-section/index.tsx
@@ -1,8 +1,30 @@
+import QuestionHeader from "~/common/ui/component/test/question-header";
+import ContainerLayout from "~/common/ui/layout/container-layout";
+
+import style from "./style/style.module.scss";
+
+const checksMock = [
+  "Анализируем ваши ответы",
+  "Персонализируем ваш план",
+  "Готовим ваши результаты",
+];
+
 const ProcessSection = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <ContainerLayout>
+        <QuestionHeader
+          title="Создаем ваш личный план похудения"
+        />
+        <div>
+          {checksMock.map((item, index) => (
+            <div key={`check_${index}`}>
+              <span>{item}</span>
+            </div>
+          ))}
+        </div>
+      </ContainerLayout>
+    </section>
   );
 };
 
