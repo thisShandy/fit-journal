1af457a5ae66a0225f20e336f7be0f777fa80956
thisShandy <shandyrix@gmail.com>
2025-08-16 11:55:46 +0200

    feat: creating result section

---
+++ client/src/common/ui/section/test/result-section/index.tsx
@@ -0,0 +1,13 @@
+import ContainerLayout from "~/common/ui/layout/container-layout";
+
+const ResultSection = () => {
+  return (
+    <section>
+      <ContainerLayout>
+
+      </ContainerLayout>
+    </section>
+  );
+};
+
+export default ResultSection;
