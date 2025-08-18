962268bb0d70a779305810aabd860a4692961c62
thisShandy <shandyrix@gmail.com>
2025-08-18 17:31:57 +0200

    feat: adding ga

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -40,6 +40,21 @@ const RootLayout = async ({
           `}
         </Script>
         {/* End Google Tag Manager */}
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-36486VE5T0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-36486VE5T0');
+          `}
+        </Script>
+        {/* End Google Analytics */}
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
--- client/src/common/model/hook/test/use-test.ts
+++ client/src/common/model/hook/test/use-test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { useState } from "react";
 
 export const useTest = () => {
@@ -5,6 +6,15 @@ export const useTest = () => {
   const [answers, setAnswers] = useState<number[]>([]);
 
   const handleNext = (answer?: number) => {
+    try {
+      // @ts-ignore
+      window.gtag("event", `st_${section + 1}`, {
+        event_category: "-",
+        event_label: `st_${section + 1}`,
+        answer: answer
+      });
+    } catch (error) {}
+
     setSection(prev => prev + 1);
     if (typeof answer === "number") setAnswers(prev => [ ...prev, answer ]);
   };
