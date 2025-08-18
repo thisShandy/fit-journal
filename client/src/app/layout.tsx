bc8b2274f10f2aee555487b4c667b1526603b652
thisShandy <shandyrix@gmail.com>
2025-08-18 16:30:24 +0200

    feat: adding gtag

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -28,19 +28,33 @@ const RootLayout = async ({
 
   return (
     <html lang={locale} className={fontConfig}>
-      <body>{children}</body>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-36486VE5T0"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-36486VE5T0');
-        `}
-      </Script>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K946HZNT');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K946HZNT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {children}
+      </body>
     </html>
   );
 };
