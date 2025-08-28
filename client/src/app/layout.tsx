ac078ac3e2b0e4b9b4984b17923363d9ad2378e5
thisShandy <shandyrix@gmail.com>
2025-08-28 11:11:43 +0200

    feat: posthog updates

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -56,29 +56,6 @@ const RootLayout = async ({
           `}
         </Script>
         {/* End Google Analytics */}
-
-        {/* MGID Pixel */}
-        <Script id="mgid-sensor" strategy="afterInteractive">
-          {`
-            (function() {
-                var d = document, w = window;
-                w.MgSensorData = w.MgSensorData || [];
-                w.MgSensorData.push({
-                    cid:934441,
-                    project: "a.mgid.com"
-                });
-                var l = "a.mgid.com";
-                var n = d.getElementsByTagName("script")[0];
-                var s = d.createElement("script");
-                s.type = "text/javascript";
-                s.async = true;
-                var dt = !Date.now?new Date().valueOf():Date.now();
-                s.src = "https://" + l + "/mgsensor.js?d=" + dt;
-                n.parentNode.insertBefore(s, n);
-            })();
-          `}
-        </Script>
-        {/* End MGID Pixel */}
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
